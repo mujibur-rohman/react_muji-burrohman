6c8c11f58680d69ca2a5a2c2a0cc504e7e7ad922
Mujiburrohman <muji.official06@gmail.com>
2022-09-06 09:41:57 +0700

    task 1 has been done

---
+++ 08_Javascript_Refreshment/Praktikum/Task_1/task_1.js
@@ -0,0 +1,31 @@
+var a = 5;
+let b = 'Kampus Merdeka';
+const nama = 'budi';
+let terdaftar = true; // ===== Jawaban B =====
+let lengkap_arr = [a, b, nama, terdaftar];
+
+function perkenalan() {
+  let asal = 'indonesia';
+  return console.log(
+    'Perkenalkan nama saya ' +
+      nama +
+      ' nomor urut ' +
+      a +
+      ' sekarang sedang mengikuti ' +
+      b +
+      ' berasal dari ' +
+      asal
+  );
+}
+
+if (terdaftar) {
+  console.log(nama + ' terdaftar sebagai kegiatan kampus merdeka');
+}
+console.log('Array = ' + lengkap_arr[2]); // ===== Jawaban A =====
+a = b;
+// nama = b;
+
+// console.log('Asal diakses = ' + nama);
+console.log('a adalah = ' + a);
+console.log('b adalah = ' + b);
+perkenalan(); // ===== Jawaban C =====
