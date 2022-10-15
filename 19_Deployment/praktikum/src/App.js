0c2a26906c044bedf10fe04fd975d6a3dfccf7c5
mujiburrohman <muji.official06@gmail.com>
2022-10-15 11:51:44 +0700

    completed deployment

---
+++ 19_Deployment/praktikum/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Selamat Datang Di Website Static Mujiburrohman</p>
+        <a
+          className="App-link"
+          href="https://github.com/mujibur-rohman/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ 19_Deployment/summary/README.md
@@ -0,0 +1,10 @@
+# Summary Deployment
+ 
+1. Poin 1
+Deployment adalah kegiatan yang bertujuan untuk menyebarkan aplikasi yang telah dikerjakan oleh para programmer. 
+
+2. Poin 2
+Pada react kita bisa menggunakan beberapa platform untuk deployment. Diantaranya adalah netlify dan surge. 
+
+3. Poin 3 
+Untuk mendeployment aplikasi react, kita harus membuild aplikasi terlebih dahulu, dengan cara npm run build.
